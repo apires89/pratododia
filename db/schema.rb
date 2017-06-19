33bf8bf43c5d95f2490a731edf8b83fa319b3c46
andre pires <andrecabralpires@gmail.com>
2017-06-19 14:46:04 +0100

    restaurnats model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619132502) do
+ActiveRecord::Schema.define(version: 20170619134349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.text     "address"
+    t.string   "city"
+    t.string   "postcode"
+    t.string   "phone"
+    t.string   "website"
+    t.string   "email"
+    t.string   "facebook"
+    t.integer  "capacity"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +48,5 @@ ActiveRecord::Schema.define(version: 20170619132502) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "restaurants", "users"
 end
