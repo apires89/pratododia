4b542e79f4a77d462da49a65792d2b61198624f8
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-06-19 20:09:23 +0100

    Landing Page skeleton

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
+require 'search.rb'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  layout "landing"
 
   def home
+    @search = SearchService.new("bacalhau")
+  end
+
+  def search
   end
 end
---
+++ app/services/search.rb
@@ -0,0 +1,5 @@
+class SearchService
+  def initialize(search_query)
+    @search_query = search_query
+  end
+end
